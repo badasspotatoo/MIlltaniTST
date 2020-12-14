67630b2da2b84a0d6248ed3bef15b7d7631f22e5
NathiqU <45428114+badasspotatoo@users.noreply.github.com>
2020-12-14 21:54:22 +0700

    update source db

--- Miltani/__init__.py
+++ Miltani/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'd2a0e39be0acfb1a5e1781d37917dd18'
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://jeernqqq:KhSoo90yLnTSeQfJMIqXUnT3C1ViegqP@suleiman.db.elephantsql.com:5432/jeernqqq'
+app.config['SECRET_KEY'] = 'rasfIJbDGbiA6ymwE_X3GYi0ymX2ndYN'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://ovxpvqzs:rasfIJbDGbiA6ymwE_X3GYi0ymX2ndYN@rajje.db.elephantsql.com:5432/ovxpvqzs'
 db = SQLAlchemy(app)
 
 from Miltani import routes
