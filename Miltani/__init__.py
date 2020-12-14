dc7b968d358dc05300f024f62f8746237ff0ceb9
NathiqU <45428114+badasspotatoo@users.noreply.github.com>
2020-12-14 22:29:03 +0700

    update db

--- Miltani/__init__.py
+++ Miltani/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'rasfIJbDGbiA6ymwE_X3GYi0ymX2ndYN'
+app.config['SECRET_KEY'] = 'd2a0e39be0acfb1a5e1781d37917dd18'
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://ovxpvqzs:rasfIJbDGbiA6ymwE_X3GYi0ymX2ndYN@rajje.db.elephantsql.com:5432/ovxpvqzs'
 db = SQLAlchemy(app)
 
